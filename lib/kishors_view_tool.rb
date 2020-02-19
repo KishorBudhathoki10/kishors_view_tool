ff4ff65924be87e474ab7ab351bf676edcc7ea12
Kishor Budhathoki <pinkfloyd@PINKFLOYDs-MacBook-Pro.local>
2020-02-19 13:25:04 +0100

    kishors initial commit

---
+++ lib/kishors_view_tool.rb
@@ -0,0 +1,7 @@
+require "kishors_view_tool/version"
+require "kishors_view_tool/renderer"
+
+module KishorsViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/kishors_view_tool/version.rb
@@ -0,0 +1,3 @@
+module KishorsViewTool
+  VERSION = "0.1.0"
+end
